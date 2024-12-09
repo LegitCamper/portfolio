b96e7fdcc0b187a83a86f58d6691bc867ac504db
sawyer bristol <sawyerbristol@gmail.com>
2024-12-08 18:13:37 -0700

    fix build file

--- build.rs
+++ build.rs
@@ -2,25 +2,25 @@ use std::path::Path;
 
 fn main() {
     let dir = std::env::var("OUT_DIR").unwrap();
-    let cargo_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
     std::process::Command::new("git")
         .args(["clone", "https://github.com/LegitCamper/resume", &dir])
         .spawn()
         .expect("Failed to clone resume repo")
         .wait()
         .unwrap();
+
+    let resume =
+        Path::new(&std::env::var("CARGO_MANIFEST_DIR").unwrap()).join("public/assets/resume.pdf");
     std::process::Command::new("typst")
         .args([
             "compile",
-            &format!("{}/resume/resume.typ", &dir),
-            &format!("{}/{}", cargo_dir, "public/assets/resume.pdf"),
+            &format!("{}/resume.typ", &dir),
+            resume.as_os_str().to_str().unwrap(),
         ])
         .spawn()
         .expect("Failed to compile resume")
         .wait()
         .unwrap();
 
-    assert!(Path::new(&cargo_dir)
-        .join("public/assets/resume.pdf")
-        .exists());
+    assert!(resume.exists());
 }
