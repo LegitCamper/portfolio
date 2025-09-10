6a7e7c46ffaeca79f03750aa9e181166d9dd55ca
sawyer bristol <sawyerbristol@gmail.com>
2025-09-10 15:03:34 -0600

    fix

--- build-games.sh
+++ build-games.sh
@@ -4,7 +4,7 @@ else
 for var in "$@"; do
         cd games/$var
         RUSTFLAGS='--cfg getrandom_backend="wasm_js"' cargo +stable build --bin $var --target wasm32-unknown-unknown --profile wasm-release
-        cp target/wasm32-unknown-unknown/wasm-release/$var.wasm ../wasm
+        cp target/wasm32-unknown-unknown/wasm-release/$var.wasm ../wasm/$var.wasm
         wasm-bindgen --no-typescript --target web \
             --out-dir ../wasm/ \
             --out-name "$var" \
